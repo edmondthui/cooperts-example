05340d43d88800bdfc56c104252005d23216f48e
Edmond Hui <edmond.t.hui@gmail.com>
2022-07-19 16:03:54 -0400

    Updates

---
+++ precache-manifest.9bdabac962ae4650a74ca0d7791c67e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f092eb1bb2232182993f84cba4f1d7",
+    "url": "/cooperts-example/index.html"
+  },
+  {
+    "revision": "6e535dc702e675d7c290",
+    "url": "/cooperts-example/static/css/main.eadd87e5.chunk.css"
+  },
+  {
+    "revision": "85c761dab71f3816eb77",
+    "url": "/cooperts-example/static/js/2.37e1e114.chunk.js"
+  },
+  {
+    "revision": "eb08861022dc5e67b9288a6f36c32650",
+    "url": "/cooperts-example/static/js/2.37e1e114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e535dc702e675d7c290",
+    "url": "/cooperts-example/static/js/main.156b12fe.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/cooperts-example/static/js/main.156b12fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f219f80a4ebe2077342",
+    "url": "/cooperts-example/static/js/runtime-main.08bcac11.js"
+  }
+]);

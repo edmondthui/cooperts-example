f8d11f3dda1857e5316332548547ba6b4b94e423
Edmond Hui <edmond.t.hui@gmail.com>
2022-08-17 17:30:49 -0400

    fix: merge conflicts

--- src/store/Reaction.tsx
+++ src/store/Reaction.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable */
 import { assertNever } from '@kofno/piper';
 import { toJS } from 'mobx';
@@ -7,15 +6,6 @@ import { addCard, deleteCard, getCards, moveCard, openDB } from '../KanbanDB';
 import Store from './index';
 import ReactionComponent, { RCProps } from './ReactionComponent';
 import { State } from './Types';
-=======
-import { observer } from "mobx-react";
-import Task from "taskarian";
-import { connectToKanbanDB, getCards } from "../utils/kanban.utils";
-import Store, { assertNever } from "./index";
-import ReactionComponent, { RCProps } from "./ReactionComponent";
-import { State } from "./Types";
-import { decodeCards } from "./Decoders";
->>>>>>> main
 
 interface Props extends RCProps<Store> {
   store: Store;
@@ -24,7 +14,6 @@ interface Props extends RCProps<Store> {
 class Reactions extends ReactionComponent<Store, State, Props> {
   tester = () => this.props.store.state;
   effect = (state: State) => {
-<<<<<<< HEAD
     console.log(state.kind, toJS(state));
     const { store } = this.props;
     switch (state.kind) {
@@ -53,27 +42,6 @@ class Reactions extends ReactionComponent<Store, State, Props> {
       case 'input-card-name':
       case 'select-card-status':
       case 'update-failed':
-=======
-    const { store } = this.props;
-    switch (state.kind) {
-      case "waiting":
-        store.load();
-        break;
-      case "loading":
-        Task.succeed<any, {}>({})
-          .assign("db", connectToKanbanDB)
-          .do(store.ready)
-          .andThen(getCards)
-          .andThen(decodeCards)
-          .do(store.setCards)
-          .fork(
-            (err) => console.log(err),
-            (success) => console.log(success)
-          );
-        break;
-      case "ready":
-      case "error":
->>>>>>> main
         break;
       default:
         assertNever(state);
